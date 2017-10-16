9bce2d73ecc779ee96f18bb3a1f774f2f5bc96e1
Elder <eldercharles@gmail.com>
2017-10-16 17:20:18 -0300

    Estrutura de Dados

--- Aula/src/ProvaAV1/tipoNo.java
+++ Aula/src/ProvaAV1/tipoNo.java
@@ -2,22 +2,9 @@
 package ProvaAV1;
 
 public class tipoNo{
+    
 	public int info;
 	public tipoNo prox;
-	
-	public tipoNo(){}
-
-    tipoNo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    tipoNo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    tipoNo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 	
 	public void setInfo(int info){
 		this.info=info;
